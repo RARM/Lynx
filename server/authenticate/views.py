430e5a70813a863c6b937d72ca20bdd8398cbd6a
HunterBresler <hunterwayneb@icloud.com>
2023-07-17 20:03:23 -0500

    g

---
+++ server/authenticate/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.contrib.auth.model import User
+
+# Create your views here.
+def signin(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        password = request.POST['password']
+        passwordConfirm = request.POST['passwordConfirm']
+
+    myuser
+
