51126e28937b9c4b6ad05ce2617cc300b5b53677
HunterBresler <hunterwayneb@icloud.com>
2023-07-22 20:33:52 -0500

    update

--- server/games/views.py
+++ server/games/views.py
@@ -9,8 +9,10 @@ def upload(request):
         if request.method == "POST":
             gameId = request.POST['gid']
             gameName = request.POST['gname']
+            gameBin = request.POST['gbin']
 
-        return JsonResponse({"message": "Your account has been created."}, status=201)
+        return JsonResponse({"message": "Your game has been uploaded.",
+                             "gname": gameName}, status=201)
     
 
 @csrf_exempt
