0fcdbd491b64119b8e66a1b65b13c8741090c5cc
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2023-07-25 12:16:42 -0400

    Adding auto-uploading script. Tested.

--- server-tests/uploading-games/index.js
+++ server-tests/uploading-games/index.js
@@ -1,39 +1,55 @@
 const sampleGames = require('./test-games.json');
-const uploadURL = 'https://api.lynxgamestore.com/games/';
+// const uploadURL = 'https://api.lynxgamestore.com/games/';
+const uploadURL = 'http://127.0.0.1:8000/games/upload';
+
+const fs = require('fs');
+const fsp = require('fs/promises');
+const mt = require('mime-types');
 
 class Game {
-    constructor(title, description) {
-        this.title = title;
-        this.description = description;
+    constructor(gameData) {
+        this.title = gameData.title;
+        this.description = gameData.description;
+        this.pathToFile = gameData.file;
+        this.desired_filename = gameData.filename;
     }
 
     async upload() {
+        const gameFile = new Blob([await fsp.readFile(this.pathToFile)], {type: mt.lookup(this.pathToFile)});
+
         const body = new FormData();
-        body.append('gname', this.title);
-        body.append('gdescr', this.description);
+        body.append('name', this.title);
+        body.append('description', this.description);
+        body.append('gbin', gameFile, this.desired_filename);
         
-        const response = await fetch(uploadURL + 'upload/', {
+        const response = await fetch(uploadURL, {
             method: 'POST',
-            data: body
+            body: body
         });
 
-        return response.ok;
+        return response;
+    }
+}
+
+function consoleLogForm(form) {
+    for (const pair of form.entries()) {
+        console.log(pair[0] + ': ' + pair[1]);
     }
 }
 
 async function main() {
     let games = new Array();
     for (let i = 0; i < sampleGames.games.length; i++) {
-        const title = sampleGames.games[i].title;
-        const descr = sampleGames.games[i].description;
-        games.push(new Game(title, descr));
+        games.push(new Game(sampleGames.games[i]));
     }
 
     let ok = true;
     for (const game of games) {
-        const good = await game.upload();
-        if (!good) {
-            console.log('Error: There was an issue uploading ' + game.title);
+        const resp = await game.upload();
+        if (!resp.ok) {
+            console.log('Error: There was an issue uploading "' + game.title + '"');
+            console.log('Error (' + resp.status + ') message: ' + resp.statusText);
+            resp.json().then(data => console.log(data));
             ok = false;
             break;
         }
