f669b2ca68878d082b29a0c48abf3fc379e221e5
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2023-08-01 23:25:27 -0400

    Create a Lynx client authentication API.
    Although this code is mainly to test the server API. It is useful
    for the Lynx client.

---
+++ server-tests/auth/HeaderUtils.js
@@ -0,0 +1,24 @@
+/**
+ * Utility class to work with request/response headers.
+**/
+export default class HeaderUtils {
+    /**
+     * Get the `sessionid` cookie from a response header containing a
+     * `Set-Cookie` header.
+     * 
+     * @param {Response.headers} headers Headers from a fetch response.
+     * @returns {string} String containing the `sessionid` to use in the `Cookie` header for future requests.
+    **/
+    static getSessionID(headers) {
+        let sessionID = '';
+        
+        for (const header of headers) {
+            if (
+                header[0] == 'set-cookie' &&
+                header[1].startsWith('sessionid')
+            ) sessionID = header[1].match(/sessionid=[0-9a-z]*;/)[0].slice(0, -1);
+        }
+    
+        return sessionID;
+    }
+}
---
+++ server-tests/auth/index.js
@@ -0,0 +1,2 @@
+import LynxAuthAPI from './LynxAuthAPI';
+
